711d42d65a56d092a5023175e0894d5bc725d625
Xin Wang <harrygun.wangxin@gmail.com>
2016-04-27 11:49:53 -0400

    update

--- src/python/kl_mode.py
+++ src/python/kl_mode.py
@@ -7,6 +7,11 @@ import pylab as pl
 
 
 
+
+
+
+
+
 def KL_modes(mask, cov_s, cov_n):
     ''' ->> contructing Karhunen-Loeve Modes <<- '''
 
@@ -14,3 +19,7 @@ def KL_modes(mask, cov_s, cov_n):
 
 
     return
+
+
+
+
