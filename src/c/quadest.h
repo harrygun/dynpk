b455868a97a9dda458c9700a8492e05a1b1a2002
Xin Wang <harrygun.wangxin@gmail.com>
2016-07-31 10:27:53 -0400

    update

---
+++ src/c/quadest.h
@@ -0,0 +1,8 @@
+#ifndef _H_QUADEST_
+#define _H_QUADEST_
+
+  void Fisher(MPIpar *mpi, double *dcov, double *icov, double *F,
+                                         size_t npix, size_t n_bp);
+
+
+#endif
