e2e86074f531d7bad71264e9512e8a3d7891e06a
Xin Wang <harrygun.wangxin@gmail.com>
2016-08-26 17:13:25 -0400

    update

---
+++ src/cpp/io.hpp
@@ -0,0 +1,10 @@
+  #ifndef _H_IO_
+  #define _H_IO_
+
+  #include "glbvarb.h"
+
+  void import_data_double(MPIpar *mpi, const char *fn, void *d, size_t size, size_t count);
+
+  void write_data(MPIpar *mpi, const char *fn, const void *d, size_t size, size_t count);
+
+  #endif
