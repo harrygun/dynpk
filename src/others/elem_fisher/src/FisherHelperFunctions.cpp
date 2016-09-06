2004c4e7884c7f94af26a4eb700ddbc7af17723a
Xin Wang <xwang@cita.utoronto.ca>
2016-09-06 15:00:00 -0400

    update

--- src/others/elem_fisher/src/FisherHelperFunctions.cpp
+++ src/others/elem_fisher/src/FisherHelperFunctions.cpp
@@ -91,7 +91,8 @@ void Combine_dcovi(std::string basename, int nmatrices, int nrows, int ncols, Di
       full_dcovi(IR(0,nrows),IR(k*nmatrices,k*nmatrices+ncols))=dCk(IR(0,nrows),IR(0,ncols));
 
       if(k==0){
-        Write(dCk(IR(0,nrows),IR(0,ncols)),"./dC0.dat", MATRIX_MARKET); 
+        Write(dCk(IR(0,nrows),IR(0,ncols)),"./dC0", MATRIX_MARKET); 
+        Write(colmatrix, "./dC0_vm", MATRIX_MARKET); 
         }
 
     }
