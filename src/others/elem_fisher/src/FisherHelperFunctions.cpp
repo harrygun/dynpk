735a9c0a586fe20c3c967f0b066dd37e656968d6
Xin Wang <xwang@cita.utoronto.ca>
2016-09-06 14:23:08 -0400

    update

--- src/others/elem_fisher/src/FisherHelperFunctions.cpp
+++ src/others/elem_fisher/src/FisherHelperFunctions.cpp
@@ -80,7 +80,7 @@ void Combine_dcovi(std::string basename, int nmatrices, int nrows, int ncols, Di
       if(k==0){
         //Write(colvectorsymm,"./dC_ext.dat"); 
 
-        ofstream fout
+        ofstream fout;
 	fout.open("dC_ext.dat", std::ios::in | std::ios::binary);
         fout.write(colvectorsymm[0], colvectorsymm.size()*sizeof(double));
         fout.close();
