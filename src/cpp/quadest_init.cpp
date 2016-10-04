7fd9143884c77ad413083e9069779d07d885a13c
Xin Wang <harrygun.wangxin@gmail.com>
2016-10-03 21:13:14 -0400

    update

--- src/cpp/quadest_init.cpp
+++ src/cpp/quadest_init.cpp
@@ -179,6 +179,8 @@
 
     // import from file //
     if (from_file) {
+      cout << "importing dcov from file " << fname << endl;
+
       Read(dcov_vec, fname);
       Display(dcov_vec, "dcov_vec");
 
