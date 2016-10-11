dd14093ad5443a0ae2778f07201bde070533433c
Xin Wang <harrygun.wangxin@gmail.com>
2016-10-10 21:46:15 -0400

    update

--- src/cpp/quadest.cpp
+++ src/cpp/quadest.cpp
@@ -111,9 +111,8 @@
     fcov_recovery(pk, covf, dcov, nbp);
     cout << "Full covariance matrix done." << endl;
 
-    string fn_out;
-    fn_out="covf.dat";
-    Write(covf, fn_out);
+    string fn_out="covf.dat";
+    Write(*covf, fn_out);
 
     // inversion //
     *covf_inv=*covf;
